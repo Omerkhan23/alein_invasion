5da30b02f1e02430ec44161c8c86bdcc45856299
Omerkhan23 <durraniaslankhan@gmail.com>
2025-03-26 19:55:35 +0500

    printed cubes of 0 to 100

--- practice.py
+++ practice.py
@@ -1,12 +1,12 @@
 square = []
 for i in range(100):
-    square.append(i*i)
+    square.append(i*i*i)
 
 x = list(range(100))
+print(square)
+#import matplotlib.pyplot as plt
 
-import matplotlib.pyplot as plt
+#plt.plot(x,square)
 
-plt.plot(x,square)
-
-plt.show()
+#plt.show()
 
